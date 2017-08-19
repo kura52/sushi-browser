878422bd446bd4f97306d3561cc253757e509692
kura52 <1qaz"WSX>
2017-08-20 08:39:48 +0900

    Mouse gesture made available on special page such as top page

--- brave/extension/extensions.js
+++ brave/extension/extensions.js
@@ -137,20 +137,24 @@ module.exports.init = (verChange) => {
 
   require('./browserAction')
 
+  let first = true
   module.exports.loadAll = function(ses){
+    loadExtension(ses,...getPath('jpkfjicglakibpenojifdiepckckakgk'),(void 0),'component')
     loadExtension(ses,'dckpbojndfoinamcdamhkjhnjnmjkfjd',getPath('default')[1],(void 0),'component')
     loadExtension(ses,...getPath('jdbefljfgobbmcidnmpjamcbhnbphjnb'),(void 0),'component')
     componentUpdater.registerComponent('jdbefljfgobbmcidnmpjamcbhnbphjnb', 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqmqh6Kxmj00IjKvjPsCtw6g2BHvKipjS3fBD0IInXZZ57u5oZfw6q42L7tgWDLrNDPvu3XDH0vpECr+IcgBjkM+w6+2VdTyPj5ubngTwvBqCIPItetpsZNJOJfrFw0OIgmyekZYsI+BsK7wiMtHczwfKSTi0JKgrwIRhHbEhpUnCxFhi+zI61p9jwMb2EBFwxru7MtpP21jG7pVznFeLV9W9BkNL1Th9QBvVs7GvZwtIIIniQkKtqT1wp4IY9/mDeM5SgggKakumCnT9D37ZxDnM2K13BKAXOkeH6JLGrZCl3aXmqDO9OhLwoch+LGb5IaXwOZyGnhdhm9MNA3hgEwIDAQAB')
     if(process.platform != 'win32'){
-      loadExtension(ses,...getPath('occjjkgifpmdgodlplnacmkejpdionan'))
+      loadExtension(ses,...getPath('occjjkgifpmdgodlplnacmkejpdionan'),(void 0),'component')
     }
 
     const appIds = fs.readFileSync(path.join(__dirname,'../../resource/extensions.txt')).toString().split(/\r?\n/)
     for(let appId of appIds) {
       if(appId.match(/^[a-z]+$/)){
+        if(!first && appId == 'niloccemoadcdkdjlinkgdfekeahmflj') continue
         loadExtension(ses,...getPath(appId))
       }
     }
+    first = false
   }
   module.exports.loadAll(session.defaultSession)
   // loadExtension(...getPath('occjjkgifpmdgodlplnacmkejpdionan'))
